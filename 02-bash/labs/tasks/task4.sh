2145f71ede4e4be5664673254ba3e54a6023a61a
Libaan <libaan-5@outlook.com>
2026-06-09 11:49:52 +0100

    Add bash task4

--- 02-bash/labs/tasks/task4.sh
+++ 02-bash/labs/tasks/task4.sh
@@ -2,17 +2,22 @@
 
 backup() {
 
-read -p "Enter a source directory: " source_dir
-mkdir /tmp/Backup
+timestamp=$(date +"Backup_%Y-%m-%d_%H-%M")
 
-if [ -d source_dir ]; then
-    if [ ! -d /tmp/Backup ]; then
-        mkdir /tmp/Backup
-    fi
+read -e -p "Enter a source directory: " source_dir
 
-    cp source_dir/*.txt /tmp/Backup
-    echo "Backup complete! Files backed up $(ls | wc -l)" 
+if [ ! -d /tmp/Backup ]; then
+    mkdir /tmp/Backup
+    echo "Backup directory created: $timestamp..."
+fi
 
+if [ ! -d "$source_dir" ]; then
+    echo "Directory not found!"
+else
+    mkdir "/tmp/Backup/$timestamp"
+    cp "$source_dir"/*.txt "/tmp/Backup/$timestamp"
+    echo "Backup completed at: $timestamp! Files backed up: $(ls "/tmp/Backup/$timestamp" | wc -l)" 
+fi
 }
 
 backup 
